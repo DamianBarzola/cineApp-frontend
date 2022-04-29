c644b756dc81565ef849d507cc0d97bd61c99c01
Agustin Etcheverry <etxeagus@gmail.com>
2022-04-29 11:41:41 -0300

    dami

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router";
 // import { get } from "../utils/httpClient";
 import {loadOneFilm} from "../actions/film";
 import Spinner from "../components/Spinner";
+import { Link } from "react-router-dom";
 
 const MovieDetails = () => {
   const { movieId } = useParams();
@@ -45,9 +46,15 @@ const MovieDetails = () => {
           <b> Descripción: </b>
           {movie.description}
         </p>
+        <Link to={"/tickets/"} >
+          <button>
+              Comprar ahora
+          </button>
+        </Link>
       </div>
     </div>
   );
 };
+
 
 export default MovieDetails;
--- src/pages/Tickets.jsx
+++ src/pages/Tickets.jsx
@@ -1,6 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/Tickets.module.css";
+import {loadFilmsAll} from "../actions/film";
+import {loadDates, loadFunciones, loadButacas} from "../actions/funcion";
+
 const Tickets = () => {
+
+  const [movies, setMovies] = useState([]);
+  const [movieId, setMovieId] =useState('');
+
+  const [fechasFunciones, setFechasFunciones] = useState([]);
+  const [fechaFuncion, setFechaFuncion] =useState('');
+
+  const [funciones, setFunciones] = useState([]);
+  const [funcion, setFuncion] =useState('');
+  
+  const [butacas, setButacas] = useState([]);
+  const [butaca, setButaca] =useState('');
+  //Pelicula, dia -> Funciones -> y butaca
+  
+  useEffect( ()=>{
+    const getMovie= async()=>{
+        const resMovie= loadFilmsAll();
+        setMovies(await resMovie);
+    }
+    getMovie();
+},[]);
+
+  const handleMovie=(event)=>{
+    setMovieId(event.target.value);
+  }
+
+  useEffect( ()=>{
+    const getDate= async()=>{
+        const resFecha= loadDates(movieId);
+        setFechasFunciones(await resFecha);
+    }
+    getDate();
+  },[movieId]);
+
+    const handleDate=(event)=>{
+      setFechaFuncion(event.target.value);
+  }
+
+  useEffect( ()=>{
+    const getFunciones= async()=>{
+        const resFuncion= loadFunciones(movieId, fechaFuncion);
+        console.log(resFuncion);
+        setFunciones(await resFuncion);
+    }
+    getFunciones();
+  },[movieId, fechaFuncion]);
+
+    const handleFuncion=(event)=>{
+      setFuncion(event.target.value);
+  }
+
+  useEffect( ()=>{
+    const getButacas= async()=>{
+        const resButacas= loadButacas(funcion.id);
+        setButacas(await resButacas);
+    }
+    getButacas();
+  },[funcion]);
+
+    const handleButaca=(event)=>{
+      setButaca(event.target.value);
+  }
+
   return (
     <div className="container">
       <div className="row d-flex justify-content-center text-center p-4 ">
@@ -10,6 +76,11 @@ const Tickets = () => {
           <div className="row">
             <div className="col-md-6">
               <h1>Foto de la Peli</h1>
+                  {/* <img
+                  className={`${styles.col} ${styles.movieImage}`}
+                  src={movie.poster}
+                  alt={movie.name}
+                  />  */}
             </div>
             <div className="col-md-6">
               <form action="">
@@ -18,11 +89,13 @@ const Tickets = () => {
                   <select
                     className="form-select bg-secondary m-2"
                     aria-label="Default select example"
-                  >
-                    <option selected>Pelicula</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+                    onChange={(e)=>handleMovie(e)}
+                    defaultValue={'DEFAULT'}
+                    >
+                  <option value="DEFAULT" disabled>-- Elegir Pelicula --</option>
+                    {movies.map((movie) => (
+                    <option value={movie.id} key={movie.id}>{movie.name}</option>
+                    ))}
                   </select>
                 </div>
 
@@ -30,11 +103,13 @@ const Tickets = () => {
                   <select
                     className="form-select bg-secondary m-2"
                     aria-label="Default select example"
-                  >
-                    <option selected>Dia</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+                    defaultValue={'DEFAULT'}
+                    onChange={(e)=>handleDate(e)}
+                    >
+                  <option value="DEFAULT" disabled>-- Elegir Fecha --</option>
+                    {fechasFunciones.map((fecha) => (
+                    <option value={fecha} key={fecha}>{fecha}</option>
+                    ))}
                   </select>
                 </div>
                 <div>
@@ -42,22 +117,26 @@ const Tickets = () => {
                     className="form-select bg-secondary m-2"
                     aria-label="Default select example"
                     required
-                  >
-                    <option selected>Hora</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+                    defaultValue={'DEFAULT'}
+                    onChange={(e)=>handleFuncion(e)}
+                    >
+                  <option value="DEFAULT" disabled>-- Elegir Funcion --</option>
+                  {funciones.map((funcion) => (
+                    <option value={funcion.id} key={funcion.id}>{funcion.horaFuncion}</option>
+                    ))}
                   </select>
                 </div>
                 <div>
                   <select
                     className="form-select bg-secondary m-2"
                     aria-label="Default select example"
-                  >
-                    <option selected>Butaca</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+                    defaultValue={'DEFAULT'} 
+                    onChange={(e)=>handleButaca(e)}
+                    >
+                  <option value="DEFAULT" disabled>-- Elegir Butaca --</option>
+                    {butacas.map((butaca) => (
+                      <option value={butaca.id} key={butaca.id}>X: {butaca.position_x} - Y: {butaca.position_y}</option>
+                      ))}
                   </select>
                 </div>
                 <button className="btn btn-primary">ASD</button>
